88ba7b253ab1a06fff9adfa672c618716361658e
Leonardo <lupadinho867@gmail.com>
2023-08-06 13:48:06 -0300

    exercicio

---
+++ src/page.js
@@ -0,0 +1,9 @@
+"use strict";
+const multi = (valor1, valor2) => {
+    return valor1 + valor2;
+};
+const nome = (nome) => {
+    console.log(`ola ${nome}`);
+};
+nome("leonardo");
+console.log(multi(10, 20));
---
+++ src/page.ts
@@ -0,0 +1,10 @@
+const multi = (valor1: number, valor2: number) => {
+    return valor1 + valor2;
+}
+const nome = (nome: String) => {
+    console.log(`ola ${nome}`);
+}
+
+
+nome("leonardo");
+console.log(multi(10,20));
